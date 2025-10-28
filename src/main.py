e1a1565ea19101cd3f84cadea3ba368a037d24c0
Kwang Myung Yu <sguys99@gmail.com>
2025-10-28 17:09:04 +0900

    :sparkles: Add get and get with query parameters
    Signed-off-by: Kwang Myung Yu <sguys99@gmail.com>

--- src/main.py
+++ src/main.py
@@ -5,3 +5,55 @@ app = FastAPI()
 @app.get("/")
 def health_check_handler():
     return {"ping": "pong"}
+
+
+# DB가 없으므로 메모리 데이터를 하나 만들자
+todo_data = {
+    1: {
+        "id": 1,
+        "contents": "실전! FastAPI 섹션 0 수강",
+        "is_done": True,
+    },
+    2: {
+        "id": 2,
+        "contents": "실전! FastAPI 섹션 1 수강",
+        "is_done": False,
+    },    
+    3: {
+        "id": 3,
+        "contents": "실전! FastAPI 섹션 2 수강",
+        "is_done": False,
+    },   
+}
+
+@app.get("/todos")
+def get_todos_handler():
+    return list(todo_data.values())
+
+
+# 쿼리 파라미터
+# get 요청 path뒤에 ? 형태로 붙은 파라미터로 추가적인 작업을 가능하도록 함
+# fastapi에서는 쿼리 파라미터를 ? 뒤에 붙이는 것이 아니라 함수 인자를 사용해서 전달함
+# @app.get("/todos")
+# def get_todos_handler(order: str):
+#     result = list(todo_data.values())
+#     if order == "DESC":
+#         return result[::-1] # 역정렬
+        
+#     return result
+# 위경우 http://127.0.0.1:8000/todos?order=DESC를 실행
+
+# order 파라미터를 optional로 받고 싶다면
+@app.get("/todos")
+def get_todos_handler(order: str| None = None):
+    result = list(todo_data.values())
+    if order and order == "DESC": # order 값이 있고 order가 DESC라면
+        return result[::-1] # 역정렬
+        
+    return result
+
+
+
+
+# 서버 실행 방법
+# uvicorn main:app
